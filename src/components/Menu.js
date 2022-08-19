9b95e3390d64515fcf09f167722a087fb5899c48
Bhupendra Patel <bhupendraptlk@gmail.com>
2022-08-20 01:37:58 +0530

    Changed Modal to Bootsrap canvas

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,33 +1,34 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import hamburger from '../images/icons/hamburgerDesktop.png';
 import close from '../images/icons/close.png';
 
 const Menu = () =>{
-  const [showMenu,setShowMenu]=useState(false);
-  const showModal = () =>{
-    setShowMenu(!showMenu);
-    var modal=document.getElementById('modal');
-    console.log(modal);
-    modal.style.left='0';
-    modal.style.opacity='100%';
-  }
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     return(
         <>
           <div className='hamburgerMenu'>
-            <button type='button' onClick={showModal}><img src={hamburger}/></button>
+            <button type='button' onClick={handleShow}>
+              <img src={hamburger}/>
+            </button>
           </div>
-          <div id="modal" className={showMenu? 'notHidden' : 'hidden'}>
-            <div className='menu-modal-content flex ac'>
-              <div className='modal-inner-menu flex fc jc'>
-                <div>ABOUT</div>
-                <div>EDUCATION</div>
-                <div>PROJECTS</div>
+          <Offcanvas className='bsCanvas' show={show} onHide={handleClose}>
+            <Offcanvas.Body>
+              <div className='flex ac'>
+                <div className='modal-inner-menu flex fc jc'>
+                  <div>ABOUT</div>
+                  <div>EDUCATION</div>
+                  <div>PROJECTS</div>
+                </div>
+                <div className='closeBtnOuter flex ac'>
+                  <button type='button' className='closeBtn' onClick={handleClose}><img src={close}/></button>
+                </div>
               </div>
-              <div className='closeBtnOuter flex ac'>
-                <button type='button' className='closeBtn' onClick={()=>setShowMenu(!showMenu)}><img src={close}/></button>
-              </div>
-            </div>
-          </div>
+            </Offcanvas.Body>
+          </Offcanvas>
         </>
     );
 }
